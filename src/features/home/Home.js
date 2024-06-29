005b3bdfeea538809eadd22aca029032b89c618a
binaypachhai <binaypachhai@gmail.com>
2024-06-29 17:17:11 +0545

    redux_toolkit_1_movieSlicer

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,40 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import Search from '../shared/Search'
-import axios from 'axios'
+import React from 'react'
+import { movieSlice } from '../movies/movieSlice'
 
 const Home = () => {
-  const [search, setSearch] = useState('spiderman');
-
-  const ab = new AbortController();
-  console.log(ab);
-
-  const getData = async (query) => {
-    try {
-      const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
-        params: {
-          qry: query,
-          api_key: '34ac47dd6534aa83110f00b143a0db1e'
-        },
-        signal: ab.signal
-      });
-      console.log(response.data);
-    } catch (error) {
-
-    }
-  }
-
-
-  useEffect(() => {
-    getData(search);
-
-    return () => {
-      ab.abort();
-    }
-
-  }, [search]);
+  console.log(movieSlice.reducer);
   return (
     <div>
-      <Search setSearch={setSearch} />
+
+
     </div>
   )
 }
